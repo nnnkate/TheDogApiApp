5bdb8dae3a3f010a534468972f570e5d4b582bf8
Ekaterina Nedelko <>
2023-08-30 11:00:32 +0300

    Initial commit

---
+++ TheDogApiApp/TheDogApiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheDogApiApp
+//
+//  Created by Ekaterina Nedelko on 30.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
